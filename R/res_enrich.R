33b95e40e4e335fd7ffc826f5e4a276bc1536e40
Slim Karkar <karkar.slim@gmail.com>
2021-06-02 18:57:51 +0200

    rebase

--- R/res_enrich.R
+++ R/res_enrich.R
@@ -18,32 +18,32 @@
 
 # function to run the fgsea function and return a dataframe of the results
 .enrichfun = function(pathways, genescores, showCategory = 10, showLeadingGenes = FALSE, fdr = TRUE, multilevel = FALSE){
-    genes=sort(genescores,decreasing=TRUE)
-    if(multilevel == FALSE){
-        fgseaRes = data.frame(fgsea(pathways, genes, minSize=2, maxSize=200,nperm=2e4))
-    } else if(multilevel == TRUE){
-        fgseaRes = data.frame(fgseaMultilevel(pathways, genes, minSize=2, maxSize=200, eps=0))
-    }
-    pv = ifelse(fdr == TRUE,"padj","pval")
-    res = fgseaRes[,c("pathway", "padj","pval", "ES")]
-    if(showLeadingGenes == TRUE){
-        res$leadingEdge = unlist(lapply(fgseaRes$leadingEdge, paste, collapse = "_"))
-    }
-    res = res[which(res[,pv]<0.05),]
-    res = res[order(res[,pv], decreasing = FALSE),]
-    respos = res[res[,"ES"]>0,]
-    if(nrow(respos)==0){
-        p = print("No significant cell-type enrichment")
-    } else {
-        respos = respos[1:min(showCategory, nrow(respos)),]
-        resf = respos
-    }
+  genes=sort(genescores,decreasing=TRUE)
+  if(multilevel == FALSE){
+    fgseaRes = data.frame(fgsea(pathways, genes, minSize=2, maxSize=200))
+  } else if(multilevel == TRUE){
+    fgseaRes = data.frame(fgseaMultilevel(pathways, genes, minSize=2, maxSize=200, eps=0))
+  }
+  pv = ifelse(fdr == TRUE,"padj","pval")
+  res = fgseaRes[,c("pathway", "padj","pval", "ES")]
+  if(showLeadingGenes == TRUE){
+    res$leadingEdge = unlist(lapply(fgseaRes$leadingEdge, paste, collapse = "_"))
+  }
+  res = res[which(res[,pv]<0.05),]
+  res = res[order(res[,pv], decreasing = FALSE),]
+  respos = res[res[,"ES"]>0,]
+  if(nrow(respos)==0){
+    p = print("No significant cell-type enrichment")
+  } else {
+    respos = respos[1:min(showCategory, nrow(respos)),]
+    resf = respos
+  }
 }
 
 enrich = function(mydata, pathways = NULL, showCategory = 10, showLeadingGenes = FALSE, fdr = TRUE, multilevel = FALSE, ICAbased = FALSE){
-    if(ICAbased == TRUE){
-        mydata = .orient_funct(mydata)
-    }
+  if(ICAbased == TRUE){
+    mydata = .orient_funct(mydata)
+  }
   apply(mydata, 2, function(x){.enrichfun(pathways = pathways, genescores = x, showCategory = showCategory, showLeadingGenes = showLeadingGenes, fdr = fdr, multilevel = multilevel)})
 }
 
